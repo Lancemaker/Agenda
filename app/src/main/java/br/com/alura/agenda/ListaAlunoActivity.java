2c08bdb6597ffe3e8eb87c539d5b4d3170efbd02
gil <lancemaker@gmail.com>
2017-08-30 17:35:02 -0300

    AndroidAgenda
    Exercícios em android (alura);

---
+++ app/src/main/java/br/com/alura/agenda/ListaAlunoActivity.java
@@ -0,0 +1,34 @@
+package br.com.alura.agenda;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.List;
+
+public class ListaAlunoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_aluno);
+        String[] alunos ={"Daniel","Ronaldo","Jeferson","Felipe","Daniel","Ronaldo","Jeferson","Felipe","Daniel","Ronaldo","Jeferson","Felipe"};
+        ListView listaAlunos = (ListView) findViewById(R.id.lista_alunos);
+        ArrayAdapter<String> adapter =  new ArrayAdapter<String>(this,android.R.layout.simple_expandable_list_item_1,alunos);
+        listaAlunos.setAdapter(adapter);
+        Button novoAluno = (Button) findViewById(R.id.novo_aluno);
+        novoAluno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentVaiProFormulario = new Intent(ListaAlunoActivity.this,FormularioActivity.class);
+                startActivity(intentVaiProFormulario);
+            }
+        });
+    }
+
+
+}
